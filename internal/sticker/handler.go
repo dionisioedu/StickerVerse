de5153f1686732fed726a85a49c643c71422bdc0
Dionisio <ceduardodfernandes@gmail.com>
2025-05-01 00:08:49 -0300

    feat: adds logs to stikers endpoint

--- internal/sticker/handler.go
+++ internal/sticker/handler.go
@@ -1,6 +1,7 @@
 package sticker
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/dionisioedu/StickerVerse/internal/user"
@@ -10,18 +11,21 @@ import (
 func ListUserStickersHandler(c *gin.Context) {
 	authUser, exists := c.Get("user")
 	if !exists {
+		log.Printf("ListUserStickersHandler user not found")
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 		return
 	}
 
 	u, ok := authUser.(*user.User)
 	if !ok {
+		log.Printf("ListUserStickersHandler invalid user context")
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid user context"})
 		return
 	}
 
 	stickers, err := ListStickersByCreator(u.ID)
 	if err != nil {
+		log.Printf("ListUserStickersHandler error: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch stickers"})
 		return
 	}

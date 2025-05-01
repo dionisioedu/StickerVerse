a0a7d3671ddf02fa551a026b106a25acec9b341f
Dionisio <ceduardodfernandes@gmail.com>
2025-05-01 00:24:04 -0300

    fix: Fixes Sticker model

--- internal/sticker/model.go
+++ internal/sticker/model.go
@@ -3,11 +3,10 @@ package sticker
 import "time"
 
 type Sticker struct {
-	ID          string    `db:"id" json:"id"`
-	CreatorID   string    `db:"creator_id" json:"creatorId"`
-	Name        string    `db:"name" json:"name"`
-	Description string    `db:"description" json:"description"`
-	ImageURL    string    `db:"image_url" json:"imageUrl"`
-	Rarity      string    `db:"rarity" json:"rarity"` // common, rare, legendary
-	CreatedAt   time.Time `db:"created_at" json:"createdAt"`
+	ID        string    `db:"id" json:"id"`
+	CreatorID string    `db:"creator_id" json:"creatorId"`
+	Title     *string   `db:"title" json:"title"`
+	ImageURL  string    `db:"image_url" json:"imageUrl"`
+	Rarity    string    `db:"rarity" json:"rarity"` // common, rare, legendary
+	CreatedAt time.Time `db:"created_at" json:"createdAt"`
 }
